5e868371fe83f072fba5533a6bfa41b8cb3af2ae
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-13 04:20:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+
+class School
+
+
+
+ROSTER = {}
+
+  def initialize(school_name)
+    @school = school_name
+  end
+
+def add_student(school, grade, student_name)
+  if school.include?(grade)
+    school[grade] << student_name
+
+  else
+    school[grade] = []
+    school[grade] << student_name
+
+  end
+end
+
+# if school includes "i" then "add to i"
+#else instance.var.get "i" then "add to i"
+
+  # def add_student(student_name, grade)
+  #   ROSTER.map do |s_name, s_grade|
+  #     s_name = student_name
+  #     s_grade = grade
+  #     ROSTER[s_grade] = s_name
+  #   end
+  # end
+
+  def roster
+    ROSTER
+  end
+
+end
